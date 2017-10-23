e54861f581ed3dc103751d973a8ad3b5e07fe76f
bmuhlestein <nd4spdoffroad@gmail.com>
2017-10-23 01:38:43 -0600

    Added textView, editText, Button, custom font for the main_login screen

--- app/src/main/java/com/example/muhlestein/haristylistcolortracker/MainLogin.java
+++ app/src/main/java/com/example/muhlestein/haristylistcolortracker/MainLogin.java
@@ -1,13 +1,22 @@
 package com.example.muhlestein.haristylistcolortracker;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainLogin extends AppCompatActivity {
+
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        t= (TextView) findViewById(R.id.my_text);
+        Typeface myCustomFont=Typeface.createFromAsset(getAssets(), "fonts/PinkClouds.ttf");
+        t.setTypeface(myCustomFont);
+
     }
 }
