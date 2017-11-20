66010f43fe6e43007f4b30b7f4a679f42d9a8528
bmuhlestein <nd4spdoffroad@gmail.com>
2017-11-19 17:16:09 -0700

    connected "New User" from main login screen to "New User Layout".  Added connect from "Submit" on "new user layout" to "ThanksRegis Layout".

--- app/src/main/java/com/example/muhlestein/haristylistcolortracker/MainLogin.java
+++ app/src/main/java/com/example/muhlestein/haristylistcolortracker/MainLogin.java
@@ -1,22 +1,36 @@
 package com.example.muhlestein.haristylistcolortracker;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainLogin extends AppCompatActivity {
 
     TextView t;
+    Button new_user_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+        new_user_btn = (Button) findViewById(R.id.new_user_btn);
 
         t= (TextView) findViewById(R.id.my_text);
         Typeface myCustomFont=Typeface.createFromAsset(getAssets(), "fonts/PinkClouds.ttf");
         t.setTypeface(myCustomFont);
+
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.new_user_btn:
+                    Intent newUser = new Intent(MainLogin.this, newUser.class);
+                    startActivity(newUser);
+                    break;
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/muhlestein/haristylistcolortracker/newUser.java
+++ app/src/main/java/com/example/muhlestein/haristylistcolortracker/newUser.java
@@ -1,17 +1,44 @@
 package com.example.muhlestein.haristylistcolortracker;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class newUser extends AppCompatActivity {
+public class newUser extends MainLogin {
+
+    EditText new_user_first_name_edit;
+    EditText new_user_last_name_edit;
+    EditText new_user_phone_edit;
+    EditText new_user_username_edit;
+    EditText new_user_password_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
 
+        Intent intent = getIntent();
 
+        new_user_first_name_edit = (EditText) findViewById(R.id.new_user_first_name_edit);
+        new_user_last_name_edit = (EditText) findViewById(R.id.new_user_last_name_edit);
+        new_user_phone_edit = (EditText) findViewById(R.id.new_user_phone_edit);
+        new_user_username_edit = (EditText) findViewById(R.id.new_user_username_edit);
+        new_user_password_edit = (EditText) findViewById(R.id.new_user_password_edit);
+
+        Button new_user_submit_btn = (Button) findViewById(R.id.new_user_submit_btn);
+        Button new_user_cancel_btn = (Button) findViewById(R.id.new_user_cancel_btn);
+
+        new_user_submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent thanks = new Intent(newUser.this, ThanksRegis.class);
+                startActivity(thanks);
+            }
+        });
     }
 }
